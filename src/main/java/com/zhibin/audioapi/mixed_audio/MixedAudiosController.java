3ac2f74a4750d474120d613ff9ed96a1b293c571
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-03 10:43:51 -0400

    Separate API logic with Business logic ------ • Use an instance of business logic service layer in the API. • Autowire the intance to make sure dependency injection is done by Spring.

--- src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
+++ src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
@@ -1,16 +1,22 @@
 package com.zhibin.audioapi.mixed_audio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/mixed_audios") /* set up endpoint */
 public class MixedAudiosController {
 
+    private final MixedAudiosService service;
+    @Autowired
+    public MixedAudiosController(MixedAudiosService service) {
+        this.service = service;
+    }
+
     public List<MixedAudios> getMixedAudios(){
-        return List.of(new MixedAudios(0, "Zhi Bin", LocalDate.now(), null));
+        return service.getMixedAudios();
     }
 }
