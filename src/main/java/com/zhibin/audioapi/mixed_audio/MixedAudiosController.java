da026928ae2977665dc8156e7d20d691d6da0ca5
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-03 13:58:53 -0400

    Change the endpoint location ------ • Make sure that @RequestMapping is the base endpoint and for different operations they are relative to @RequestMapping

--- src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
+++ src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
@@ -1,13 +1,16 @@
 package com.zhibin.audioapi.mixed_audio;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/mixed_audios") /* set up endpoint */
+@RequestMapping(path ="/api/v1/mixed_audios") /* set up base endpoint. Any other
+                                                endpoint inside this class is relative
+                                                to this endpoint */
 public class MixedAudiosController {
 
     private final MixedAudiosService service;
@@ -16,6 +19,7 @@ public class MixedAudiosController {
         this.service = service;
     }
 
+    @GetMapping("/all")
     public List<MixedAudios> getMixedAudios(){
         return service.getMixedAudios();
     }
