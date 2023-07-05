60e1b6eca991cf9297e14fe8a41da1f44ba91faa
Zhi Bin <105875767+zhibin-wang09@users.noreply.github.com>
2023-07-05 17:06:27 -0400

    Add more CRUD operations ----- • Add a post operation that allow the api the accept a json body with fields the same as MixedAudios • Add a delete operation that allow the api to delete a MixedAudio entity in the database base on the id

--- src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
+++ src/main/java/com/zhibin/audioapi/mixed_audio/MixedAudiosController.java
@@ -1,11 +1,14 @@
 package com.zhibin.audioapi.mixed_audio;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path ="/api/v1/mixed_audios") /* set up base endpoint. Any other
@@ -22,5 +25,19 @@ public class MixedAudiosController {
     @GetMapping("/all")
     public List<MixedAudios> getMixedAudios(){
         return service.getMixedAudios();
+    }
+
+    @PostMapping
+    public ResponseEntity uploadProduction(@RequestBody MixedAudios audios){
+        audios.setDate(LocalDate.now());
+        service.addNewMixedAudio(audios);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{audioId}")
+    public void deleteProduction(@PathVariable("audioId") Long id){
+        Optional<MixedAudios> audio = service.findAudioById(id);
+        if(audio.isEmpty()) throw new NoSuchElementException();
+        service.deleteMixedAudio(id);
     }
 }
